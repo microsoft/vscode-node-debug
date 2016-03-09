6fbad7ac6085d5f11abc80681519c5fe55e721e8
Andre Weinand <aweinand@microsoft.com>
2016-03-09 16:43:07 +0100

    cleanup typings

--- src/typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="node/node.d.ts" />
-/// <reference path="es6-promise/es6-promise.d.ts" />
-/// <reference path="es6-collections/es6-collections.d.ts" />
-/// <reference path="source-map/source-map.d.ts" />
