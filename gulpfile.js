ff0ff57300d9da74f35fc1f68f63004beb23fa26
Andre Weinand <aweinand@microsoft.com>
2016-07-03 00:38:30 +0200

    Revert "add inlined source"
    This reverts commit 30bed49acf3d602c78c274236f454ba4762ae942.

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ var tsProject = ts.createProject('./src/tsconfig.json');
 var nls = require('vscode-nls-dev');
 
 var inlineMap = true;
-var inlineSource = true;
+var inlineSource = false;
 
 var watchedSources = [
 	'src/**/*',
