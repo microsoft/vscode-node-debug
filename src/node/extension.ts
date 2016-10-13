1dea3ba9fe2413f9605b3448e1b86efdd5ce67e9
isidor <inikolic@microsoft.com>
2016-10-13 11:19:04 +0200

    use more specific name for command for initial configurations

--- src/node/extension.ts
+++ src/node/extension.ts
@@ -185,7 +185,7 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	context.subscriptions.push(pickNodeProcess);
 
-	context.subscriptions.push(vscode.commands.registerCommand('extension.provideInitialConfigurations', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('extension.node.provideInitialConfigurations', () => {
 		const packageJsonPath = join(vscode.workspace.rootPath, 'package.json');
 		let program = vscode.workspace.textDocuments.some(document => document.languageId === 'typescript') ? 'app.ts' : null;
 
