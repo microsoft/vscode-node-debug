65c75a2eb7357a59424d15c2a44499276aca9c7e
Erich Gamma <egamma@microsoft.com>
2017-05-12 11:48:54 +0200

    Update tslint to the latest

--- gulpfile.js
+++ gulpfile.js
@@ -173,27 +173,17 @@ var allTypeScript = [
 
 var tslintFilter = [
 	'**',
-	'!**/*.d.ts',
-	'!**/typings/**'
+	'!**/*.d.ts'
 ];
-
-var lintReporter = function (output, file, options) {
-	//emits: src/helloWorld.c:5:3: warning: implicit declaration of function ‘prinft’
-	var relativeBase = file.base.substring(file.cwd.length + 1).replace('\\', '/');
-	output.forEach(function(e) {
-		var message = relativeBase + e.name + ':' + (e.startPosition.line + 1) + ':' + (e.startPosition.character + 1) + ': ' + e.failure;
-		console.log('[tslint] ' + message);
-	});
-};
 
 gulp.task('tslint', function () {
 	gulp.src(allTypeScript)
 	.pipe(filter(tslintFilter))
 	.pipe(tslint({
+		formatter: "prose",
 		rulesDirectory: "node_modules/tslint-microsoft-contrib"
 	}))
-	.pipe(tslint.report(lintReporter, {
-		summarizeFailureOutput: false,
+	.pipe(tslint.report( {
 		emitError: false
 	}))
 });
