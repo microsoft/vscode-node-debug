11efc1d05452cc7ae79ae9fe320de0af93ecdc78
Andre Weinand <aweinand@microsoft.com>
2018-02-07 10:59:07 +0100

    simplify by using new 'when' patterns

--- src/node/extension/extension.ts
+++ src/node/extension/extension.ts
@@ -24,11 +24,7 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('extension.pickNodeProcess', () => pickProcess()));
 
 	// loaded scripts
-	const provider = new LoadedScriptsProvider(context);
-	vscode.window.registerTreeDataProvider('extension.node-debug.loadedScriptsExplorer.node', provider);
-	vscode.window.registerTreeDataProvider('extension.node-debug.loadedScriptsExplorer.node2', provider);
-	vscode.window.registerTreeDataProvider('extension.node-debug.loadedScriptsExplorer.extensionHost', provider);
-	vscode.window.registerTreeDataProvider('extension.node-debug.loadedScriptsExplorer.chrome', provider);
+	vscode.window.registerTreeDataProvider('extension.node-debug.loadedScriptsExplorer', new LoadedScriptsProvider(context));
 	context.subscriptions.push(vscode.commands.registerCommand('extension.node-debug.pickLoadedScript', () => pickLoadedScript()));
 	context.subscriptions.push(vscode.commands.registerCommand('extension.node-debug.openScript', (session: vscode.DebugSession, source) => openScript(session, source)));
 
