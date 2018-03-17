b821da8bb78f50744595227f2604a3025e0249e1
André Weinand <aweinand@microsoft.com>
2018-03-17 20:46:36 +0100

    support process picker on linux

--- src/node/extension/processTree.ts
+++ src/node/extension/processTree.ts
@@ -81,7 +81,7 @@ export function getProcesses(one: (pid: number, ppid: number, command: string, a
 				}
 			}));
 
-		} else {	// OS X & Linux
+		} else if (process.platform === 'darwin') {	// OS X
 
 			proc = spawn('/bin/ps', [ '-x', '-o', `pid,ppid,comm=${'a'.repeat(256)},command` ]);
 			proc.stdout.setEncoding('utf8');
@@ -91,6 +91,35 @@ export function getProcesses(one: (pid: number, ppid: number, command: string, a
 				const ppid = Number(line.substr(6, 5));
 				const command = line.substr(12, 256).trim();
 				const args = line.substr(269 + command.length);
+
+				if (!isNaN(pid) && !isNaN(ppid)) {
+					one(pid, ppid, command, args);
+				}
+			}));
+
+		} else {	// linux
+
+			proc = spawn('/bin/ps', [ '-ax', '-o', 'pid,ppid,comm:20,command' ]);
+			proc.stdout.setEncoding('utf8');
+			proc.stdout.on('data', lines(line => {
+
+				const pid = Number(line.substr(0, 5));
+				const ppid = Number(line.substr(6, 5));
+				let command = line.substr(12, 20).trim();
+				let args = line.substr(33);
+
+				let pos = args.indexOf(command);
+				if (pos >= 0) {
+					pos = pos + command.length;
+					while (pos < args.length) {
+						if (args[pos] === ' ') {
+							break;
+						}
+						pos++;
+					}
+					command = args.substr(0, pos)
+					args = args.substr(pos + 1);
+				}
 
 				if (!isNaN(pid) && !isNaN(ppid)) {
 					one(pid, ppid, command, args);
