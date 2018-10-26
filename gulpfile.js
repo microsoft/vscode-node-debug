c247d0bcc1a75b3d62d18c854be344cfb97243b5
Andre Weinand <aweinand@microsoft.com>
2018-10-26 17:10:09 +0200

    support package & publish as webpack

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,7 @@ var filter = require('gulp-filter');
 var uglify = require('gulp-uglify');
 var del = require('del');
 var runSequence = require('run-sequence');
-var es = require('event-stream');
 var typescript = require('typescript');
-var cp = require('child_process');
-var vsce = require('vsce');
 
 var tsProject = ts.createProject('./src/tsconfig.json', { typescript });
 var nls = require('vscode-nls-dev');
@@ -45,17 +42,6 @@ const transifexApiToken = process.env.TRANSIFEX_API_TOKEN;
 const transifexProjectName = 'vscode-extensions';
 const transifexExtensionName = 'vscode-node-debug';
 
-const defaultLanguages = [
-	{ id: 'zh-tw', folderName: 'cht', transifexId: 'zh-hant' },
-	{ id: 'zh-cn', folderName: 'chs', transifexId: 'zh-hans' },
-	{ id: 'ja', folderName: 'jpn' },
-	{ id: 'ko', folderName: 'kor' },
-	{ id: 'de', folderName: 'deu' },
-	{ id: 'fr', folderName: 'fra' },
-	{ id: 'es', folderName: 'esn' },
-	{ id: 'ru', folderName: 'rus' },
-	{ id: 'it', folderName: 'ita' }
-];
 
 gulp.task('default', function(callback) {
 	runSequence('build', callback);
@@ -66,19 +52,15 @@ gulp.task('compile', function(callback) {
 });
 
 gulp.task('build', function(callback) {
-	runSequence('clean', 'internal-nls-build', callback);
-});
-
-gulp.task('publish', function(callback) {
-	runSequence('build', 'add-i18n', 'vsce-publish', callback);
-});
-
-gulp.task('package', function(callback) {
-	runSequence('build', 'add-i18n', 'vsce-package', callback);
+	runSequence('clean', 'internal-build', callback);
 });
 
 gulp.task('clean', function() {
-	return del(['out/**', 'package.nls.*.json', 'node-debug-*.vsix']);
+	return del(['out/**', 'dist/**', 'package.nls.*.json', 'node-debug-*.vsix']);
+})
+
+gulp.task('prepare-for-webpack', function(callback) {
+	runSequence('clean', 'internal-minify-scripts', 'nls-bundle-create', callback);
 })
 
 gulp.task('watch', ['internal-build'], function(cb) {
@@ -93,10 +75,6 @@ gulp.task('internal-build', function(callback) {
 	runSequence('internal-compile', 'internal-copy-scripts', 'internal-minify-scripts', callback);
 });
 
-gulp.task('internal-nls-build', function(callback) {
-	runSequence('internal-nls-compile', 'internal-copy-scripts', 'internal-minify-scripts', callback);
-});
-
 gulp.task('internal-copy-scripts', function() {
 	return gulp.src(scripts)
 		.pipe(gulp.dest(outDest + '/node'));
@@ -108,14 +86,10 @@ gulp.task('internal-minify-scripts', function() {
 		.pipe(gulp.dest(outDest + '/node'));
 });
 
-function compile(buildNls) {
+function compile() {
 	var r = tsProject.src()
 		.pipe(sourcemaps.init())
-		.pipe(tsProject()).js
-		.pipe(buildNls ? nls.rewriteLocalizeCalls() : es.through())
-		.pipe(buildNls ? nls.createAdditionalLanguageFiles(defaultLanguages, 'i18n', 'out') : es.through())
-		.pipe(buildNls ? nls.bundleMetaDataFiles('ms-vscode.node-debug', 'out') : es.through())
-		.pipe(buildNls ? nls.bundleLanguageFiles() : es.through());
+		.pipe(tsProject()).js;
 
 	if (inlineMap && inlineSource) {
 		r = r.pipe(sourcemaps.write());
@@ -132,18 +106,20 @@ function compile(buildNls) {
 }
 
 gulp.task('internal-compile', function() {
-	return compile(false);
+	return compile();
 });
 
-gulp.task('internal-nls-compile', function() {
-	return compile(true);
-});
+gulp.task('nls-bundle-create', function () {
+	var r = tsProject.src()
+		.pipe(sourcemaps.init())
+		.pipe(tsProject()).js
+		.pipe(nls.createMetaDataFiles())
+		.pipe(nls.bundleMetaDataFiles('ms-vscode.node-debug', 'out'))
+		.pipe(nls.bundleLanguageFiles())
+		.pipe(filter('**/nls.*.json'));
 
-gulp.task('add-i18n', function() {
-	return gulp.src(['package.nls.json'])
-		.pipe(nls.createAdditionalLanguageFiles(defaultLanguages, 'i18n'))
-		.pipe(gulp.dest('.'));
-});
+	return r.pipe(gulp.dest('dist'));
+})
 
 gulp.task('transifex-push', ['build'], function() {
 	return gulp.src(['package.nls.json', 'out/nls.metadata.header.json','out/nls.metadata.json'])
@@ -157,29 +133,6 @@ gulp.task('transifex-push-test', ['build'], function() {
 		.pipe(gulp.dest(path.join('..', `${transifexExtensionName}-push-test`)));
 });
 
-
-gulp.task('transifex-pull', function() {
-	return es.merge(defaultLanguages.map(function(language) {
-		return nls.pullXlfFiles(transifexApiHostname, transifexApiName, transifexApiToken, language, [{ name: transifexExtensionName, project: transifexProjectName }]).
-			pipe(gulp.dest(`../${transifexExtensionName}-localization/${language.folderName}`));
-	}));
-});
-
-gulp.task('i18n-import', function() {
-	return es.merge(defaultLanguages.map(function(language) {
-		return gulp.src(`../${transifexExtensionName}-localization/${language.folderName}/**/*.xlf`)
-			.pipe(nls.prepareJsonFiles())
-			.pipe(gulp.dest(path.join('./i18n', language.folderName)));
-	}));
-});
-
-gulp.task('vsce-publish', function() {
-	return vsce.publish();
-});
-
-gulp.task('vsce-package', function() {
-	return vsce.createVSIX();
-});
 
 var allTypeScript = [
 	'src/**/*.ts'
